e0eb00d2a9c65f0edfaccb8fb70ff61dcebf1b91
HeRoMo <hero@asterisk-works.jp>
2018-04-14 08:47:05 +0900

    update to webpack4

--- webpack.config.js
+++ webpack.config.js
@@ -5,19 +5,31 @@ const nodeExternals = require('webpack-node-externals');
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
-  devtool: 'source-map',
+  mode: slsw.lib.webpack.isLocal ? 'development': 'production',
+  optimization: {
+    // We no not want to minimize our code.
+    minimize: false
+  },
+  performance: {
+    // Turn off size warnings for entry points
+    hints: false
+  },
+  devtool: 'nosources-source-map',
   externals: [nodeExternals()],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel-loader'],
-      include: __dirname,
-      exclude: /node_modules/,
-    }]
+      rules: [
+        {
+          test: /\.js$/,
+          use: [{loader: 'babel-loader'}],
+          include: __dirname,
+          exclude: /node_modules/,
+        }
+    ]
   },
   output: {
-    libraryTarget: 'commonjs',
+    libraryTarget: 'commonjs2',
     path: path.join(__dirname, '.webpack'),
-    filename: '[name].js'
-  }
+    filename: '[name].js',
+    sourceMapFilename: '[file].map'
+  },
 };
